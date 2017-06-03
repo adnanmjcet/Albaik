fa8b0146f90a39c2cf1a68f43b52fc4e712a9e75
adnanmjcet <adnanmjcet@hotmail.com>
2017-06-03 11:24:59 +0530

    StudentModel Uploaded

---
+++ BOL/CommonModel/StudentModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL.CommonModel
+{
+    public partial class StudentModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Branch { get; set; }
+
+        public string Phoneno { get; set; }
+
+        public string AcademicYear { get; set; }
+
+        public byte[] Photo { get; set; }
+
+        public List<StudentModel> StudentModelList { get; set; }
+    }
+}
